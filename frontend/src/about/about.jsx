8578b4dfe085d382a72b3c954f662fe102b47c97
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2018-03-27 23:07:31 -0300

    Inserting page About

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>About</h1>
+        <PageHeader name='About' small='Us' />
+
+        <h2>Our Story</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi impedit nihil, doloremque, assumenda neque amet animi nisi, fugiat quibusdam harum dolores obcaecati. Iste, eum neque? Ipsum adipisci odit dolorum officiis?</p>
+        <p>Provident, quisquam doloremque hic ad ullam magni eum vitae ipsa neque dolorem excepturi eos. Sequi animi saepe amet nemo obcaecati neque harum voluptates ducimus atque, cum debitis veritatis! Voluptate, iusto!</p>
+        <p>Eos cupiditate corrupti a libero, aliquam error velit eaque. Aspernatur expedita itaque harum facilis accusantium eius maxime eos illo quam consequuntur! Et tempore nisi incidunt dicta, porro maiores exercitationem totam.</p>
+        <p>Eum neque quasi adipisci cumque? Sequi quod neque explicabo ipsum ipsam? Labore sit ullam reprehenderit voluptatum cumque ratione. Reiciendis asperiores nostrum ipsum repellendus eaque. Aperiam architecto dolorum sapiente aliquid quibusdam?</p>
+        <p>Cumque modi molestiae sapiente sunt rem, repudiandae aspernatur blanditiis non. Esse eos sit eum, necessitatibus harum, perferendis nemo ipsa ducimus inventore minus natus laudantium aliquid cum voluptatum? Autem, eos saepe.</p>
+
+        <h2>Mission and Value Proposition</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi impedit nihil, doloremque, assumenda neque amet animi nisi, fugiat quibusdam harum dolores obcaecati. Iste, eum neque? Ipsum adipisci odit dolorum officiis?</p>        
+        <p>Eos cupiditate corrupti a libero, aliquam error velit eaque. Aspernatur expedita itaque harum facilis accusantium eius maxime eos illo quam consequuntur! Et tempore nisi incidunt dicta, porro maiores exercitationem totam.</p>
+        <p>Eum neque quasi adipisci cumque? Sequi quod neque explicabo ipsum ipsam? Labore sit ullam reprehenderit voluptatum cumque ratione. Reiciendis asperiores nostrum ipsum repellendus eaque. Aperiam architecto dolorum sapiente aliquid quibusdam?</p>
+        <p>Cumque modi molestiae sapiente sunt rem, repudiandae aspernatur blanditiis non. Esse eos sit eum, necessitatibus harum, perferendis nemo ipsa ducimus inventore minus natus laudantium aliquid cum voluptatum? Autem, eos saepe.</p>
     </div>
 )
