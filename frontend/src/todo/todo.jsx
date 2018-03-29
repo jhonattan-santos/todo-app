3235864a37ef51f7a77392e331540f2bdc80c772
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2018-03-28 22:01:02 -0300

    Inserting file to Todo app

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader'
-
+import TodoForm from './todoForm'
+import TodoList from './todoList'
 export default class Todo extends Component {
     render() {
         return (
             <div>
                 <PageHeader name='Task`s' small='Register'/>
+                <TodoForm />
+                <TodoList />
             </div>
         )
     }
