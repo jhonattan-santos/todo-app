6b81bd547ce20922a50aaf4d4fb53c09bbd24dc9
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2018-03-31 10:50:16 -0300

    Integrate the frontend with backend using axios

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -5,10 +5,20 @@ import IconButton from '../template/iconButton'
 export default props => (
     <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
-            <input type='text' name='Insert a task' id='description' className='form-control' placeholder='Insert a task...'/>
+            <input 
+                type='text' 
+                name='Insert a task' 
+                id='description' 
+                className='form-control' 
+                placeholder='Insert a task...'
+                onChange={props.handleChange}
+                value={props.description}/>
         </Grid>
         <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus'></IconButton>            
+            <IconButton 
+                style='primary' 
+                icon='plus' 
+                onClick={props.handleAdd}></IconButton>            
         </Grid>
     </div>
 )
